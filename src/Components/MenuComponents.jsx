c314ad74335daf0875d033a62aadece86e07f918
WalterX95 <walter_1995@hotmail.it>
2025-02-15 17:02:08 +0100

    Prove Editor Di testo per Note

--- src/Components/MenuComponents.jsx
+++ src/Components/MenuComponents.jsx
@@ -1,23 +1,23 @@
-import {Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import  menuData  from "../data/menuData.json";
-import { FaAndroid } from "react-icons/fa";
+import {Container, Nav, Navbar } from "react-bootstrap";
+import { BrowserRouter, Routes, Route } from "react-router";
+import CreateNote from "./CreateNote";
 
 const MenuComponent = ({title}) => {
      return (
-        <Navbar collapseOnSelect expand="md" className="bg-body-tertiary" bg="dark">
+        <Navbar collapseOnSelect expand="md" className="bg-body-dark text-white" bg="dark">
         <Container>
-          <Navbar.Brand href="#home">{title}</Navbar.Brand>
+          <Navbar.Brand className="text-white" href="#home">{title}</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ms-auto">
-              <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-                <FaAndroid/>
-                {menuData.map((link) => {
-                    return(
-                        <NavDropdown.Item key={link.id} href={link.link}>{link.label}</NavDropdown.Item>
-                    )
-                })}
-              </NavDropdown>
+            <Nav.Link className="text-white" href="#features">Features</Nav.Link>
+            <BrowserRouter>
+            <Routes>
+                    <Route index element={<CreateNote/>} />
+                    <Route path="about" element={<></>} />
+                    
+                  </Routes>
+            </BrowserRouter>
             </Nav>
           </Navbar.Collapse>
         </Container>
